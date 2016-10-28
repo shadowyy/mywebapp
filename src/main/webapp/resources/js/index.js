0917e94e3208c942e3fad46490bfc580bc9853d4
madness <yy225600@163.com>
2016-10-29 00:53:26 +0800

    springmvc json交互解决@ResponseBody 415问题

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -4,18 +4,18 @@ $(function () {
         age: 22,
         name: "jack"
     };
-    // $.getJSON("/mywebapp/list/queryUser", JSON.stringify(data), function (json) {
-    //     $('#info').html(json);
-    // });
-    $.ajax({
-        type: "GET",
-        url: "/mywebapp/list/queryUser",
-        data: data,
-        contentType: "application/json;charset=UTF-8",
-        dataType: "json",
-        success: function (json) {
-            $('#info').html(json);
-        }
+    $("button").click(function(){
+        $.ajax({
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            url: "/mywebapp/list/queryUser",
+            data: JSON.stringify(data),
+            success: function (json) {
+                $('#info').html(json);
+            }
+        });
     });
+
 }());
 
