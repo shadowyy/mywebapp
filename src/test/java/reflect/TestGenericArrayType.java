e293abea6efa1064e986f7687ae2c525d4f56777
shadowyy <yy225600@gmail.com>
2017-09-06 11:04:24 +0800

    reflect

--- src/test/java/reflect/TestGenericArrayType.java
+++ src/test/java/reflect/TestGenericArrayType.java
@@ -22,16 +22,13 @@ import java.util.List;
  */
 public class TestGenericArrayType<T> {
     public static void main(String[] args) throws Exception {
-        Method method = Test.class.getDeclaredMethods()[0];
-        System.out.println(method);// public void com.test.Test.show(java.util.List[],java.lang.Object[],java.util.List,java.lang.String[],int[])
-        Type[] types = method.getGenericParameterTypes();  // 这是 Method 中的方法
+        Method method = TestGenericArrayType.class.getDeclaredMethods()[1];
+        Type[] types = method.getGenericParameterTypes();
         for (Type type : types) {
             System.out.println(type instanceof GenericArrayType);
         }
     }
-}
 
-class Test<T> {
     public void show(List<String>[] pTypeArray, T[] vTypeArray, List<String> list, String[] strings, int[] ints) {
     }
 }
--- src/test/java/reflect/TestParameterizedType.java
+++ src/test/java/reflect/TestParameterizedType.java
@@ -1,5 +1,8 @@
 package reflect;
 
+import com.google.common.reflect.TypeToken;
+import org.springframework.core.ParameterizedTypeReference;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -18,13 +21,23 @@ public class TestParameterizedType {
     Map<String, String> map;
 
     public static void main(String[] args) throws Exception {
-        Field f = TestParameterizedType.class.getDeclaredField("map");
-        System.out.println(f.getGenericType() instanceof ParameterizedType);  // true
-        ParameterizedType pType = (ParameterizedType) f.getGenericType();
+        Field field = TestParameterizedType.class.getDeclaredField("map");
+        ParameterizedType pType = (ParameterizedType) field.getGenericType();
         System.out.println(pType.getRawType());                               // interface java.util.Map
         for (Type type : pType.getActualTypeArguments()) {
             System.out.println(type);
         }
         System.out.println(pType.getOwnerType());                             // 返回是谁的member null
+
+        //获取type
+        Type type=new ParameterizedTypeReference<Map<String,Integer>>(){}.getType();//type instanceof sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl
+        System.out.println(type);
+
+        TypeToken<Map<String,Integer>> typeToken= new TypeToken<Map<String,Integer>>(){};
+        typeToken.getRawType();
+        typeToken.getType();
+        typeToken.getTypes();
+        typeToken.wrap();
+        System.out.println();
     }
 }
